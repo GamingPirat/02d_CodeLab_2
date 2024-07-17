a1320549ad5048d40315259f7ebf5f109620785d
Michael <GamingPirat@gmail.com>
2024-07-17 14:07:25 +0200

    01 / Codechellange

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,17 +1,30 @@
+// Datei: App.java
 package com.cc.java;
 
 public class App {
 
-	public static void main(String[] args) {
-	
-	
+    public static void main(String[] args) {
+        Mitarbeiter mitarbeiter1 = new Mitarbeiter("Schneider", "Anna", "Manager", 2015);
+        Mitarbeiter mitarbeiter2 = new Mitarbeiter("Bauer", "Hans", "Developer", 2018);
+        Mitarbeiter mitarbeiter3 = new Mitarbeiter("Martinelli", "Silvia", "CEO", 2020);
 
-	}
+        output(mitarbeiter1.getInfo("familyname")); // Schneider
+        output(mitarbeiter1.getInfo("firstname"));  // Anna
+        output(mitarbeiter1.getInfo("role"));       // Manager
+        output(mitarbeiter1.getInfo("yearofentry"));// 2015
 
-	
+        output(mitarbeiter2.getInfo("familyname")); // Bauer
+        output(mitarbeiter2.getInfo("firstname"));  // Hans
+        output(mitarbeiter2.getInfo("role"));       // Developer
+        output(mitarbeiter2.getInfo("yearofentry"));// 2018
 
-	private static void output(String outStr) {
-		System.out.println(outStr);
-	}
+        output(mitarbeiter3.getInfo("familyname")); // Martinelli
+        output(mitarbeiter3.getInfo("firstname"));  // Silvia
+        output(mitarbeiter3.getInfo("role"));       // CEO
+        output(mitarbeiter3.getInfo("yearofentry"));// 2020
+    }
 
-};
+    private static void output(String outStr) {
+        System.out.println(outStr);
+    }
+}
---
+++ com/cc/java/Mitarbeiter.java
@@ -0,0 +1,31 @@
+// Datei: Mitarbeiter.java
+package com.cc.java;
+
+public class Mitarbeiter {
+    private String familyName;
+    private String firstName;
+    private String role;
+    private int yearOfEntry;
+
+    public Mitarbeiter(String familyName, String firstName, String role, int yearOfEntry) {
+        this.familyName = familyName;
+        this.firstName = firstName;
+        this.role = role;
+        this.yearOfEntry = yearOfEntry;
+    }
+
+    public String getInfo(String property) {
+        switch (property.toLowerCase()) {
+            case "familyname":
+                return familyName;
+            case "firstname":
+                return firstName;
+            case "role":
+                return role;
+            case "yearofentry":
+                return String.valueOf(yearOfEntry);
+            default:
+                return "Property not found";
+        }
+    }
+}
