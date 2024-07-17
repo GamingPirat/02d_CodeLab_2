94e5193a5f727596f5ea94a2e839d4e4e1e8ddda
Michael <GamingPirat@gmail.com>
2024-07-17 16:27:32 +0200

    02 / Codechellange - text nebeneinander

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,31 +4,54 @@ package com.cc.java;
 public class App {
 
     public static void main(String[] args) {
-        Mitarbeiter mitarbeiter1 = new Mitarbeiter("Schneider", "Anna", "Manager", 2015);
-        Mitarbeiter mitarbeiter2 = new Mitarbeiter("Bauer", "Hans", "Developer", 2018);
-        Mitarbeiter mitarbeiter3 = new Mitarbeiter("Martinelli", "Silvia", "CEO", 2020);
+        Mitarbeiter mitarbeiter1 = new Mitarbeiter("Schneider ", "Anna ", "Manager ", 2015);
+        Mitarbeiter mitarbeiter2 = new Mitarbeiter("Bauer ", "Hans ", "Developer ", 2018);
+        Mitarbeiter mitarbeiter3 = new Mitarbeiter("Martinelli ", "Silvia ", "CEO ", 2020);
 
-        output(mitarbeiter1.getInfo("familyname")); // Schneider
-        output(mitarbeiter1.getInfo("firstname"));  // Anna
-        output(mitarbeiter1.getInfo("role"));       // Manager
-        output(mitarbeiter1.getInfo("yearofentry"));// 2015
+        // output(mitarbeiter1.getInfo("familyname")); // Schneider
+        // output(mitarbeiter1.getInfo("firstname"));  // Anna
+        // output(mitarbeiter1.getInfo("role"));       // Manager
+        // output(mitarbeiter1.getInfo("yearofentry"));// 2015
+        
+        // System.out.println("-----------------");
+
+
+        System.out.print(mitarbeiter1.getInfo("familyname"));
+        System.out.print(mitarbeiter1.getInfo("firstname"));
+        System.out.print(mitarbeiter1.getInfo("role"));
+        System.out.println(mitarbeiter1.getInfo("yearofentry"));
 
         System.out.println("-----------------");
 
-        output(mitarbeiter2.getInfo("familyname")); // Bauer
-        output(mitarbeiter2.getInfo("firstname"));  // Hans
-        output(mitarbeiter2.getInfo("role"));       // Developer
-        output(mitarbeiter2.getInfo("yearofentry"));// 2018
+        // output(mitarbeiter2.getInfo("familyname")); // Bauer
+        // output(mitarbeiter2.getInfo("firstname"));  // Hans
+        // output(mitarbeiter2.getInfo("role"));       // Developer
+        // output(mitarbeiter2.getInfo("yearofentry"));// 2018
+
+        // System.out.println("-----------------");
+
+        System.out.print(mitarbeiter2.getInfo("familyname"));
+        System.out.print(mitarbeiter2.getInfo("firstname"));
+        System.out.print(mitarbeiter2.getInfo("role"));
+        System.out.println(mitarbeiter2.getInfo("yearofentry"));
 
         System.out.println("-----------------");
 
-        output(mitarbeiter3.getInfo("familyname")); // Martinelli
-        output(mitarbeiter3.getInfo("firstname"));  // Silvia
-        output(mitarbeiter3.getInfo("role"));       // CEO
-        output(mitarbeiter3.getInfo("yearofentry"));// 2020
+        // output(mitarbeiter3.getInfo("familyname")); // Martinelli
+        // output(mitarbeiter3.getInfo("firstname"));  // Silvia
+        // output(mitarbeiter3.getInfo("role"));       // CEO
+        // output(mitarbeiter3.getInfo("yearofentry"));// 2020
+
+        // System.out.println("-----------------");
+
+        System.out.print(mitarbeiter3.getInfo("familyname"));
+        System.out.print(mitarbeiter3.getInfo("firstname"));
+        System.out.print(mitarbeiter3.getInfo("role"));
+        System.out.println(mitarbeiter3.getInfo("yearofentry"));
+
     }
 
-    private static void output(String outStr) {
-        System.out.println(outStr);
-    }
+    // private static void output(String outStr) {
+    //     System.out.println(outStr);
+    // }
 }
